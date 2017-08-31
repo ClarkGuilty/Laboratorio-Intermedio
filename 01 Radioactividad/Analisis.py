f0d004996de58a108ae310ab47cd29c899479f27
Javier Acevedo <Javier Acevedo>
2017-08-30 23:44:19 -0500

    Hay que volver a digitalizar los datos

---
+++ 01 Radioactividad/Analisis.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 30 23:16:46 2017
+
+@author: Javier Alejandro Acevedo Barroso
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+rFondo = 0.416 #Conteos por segundo
+
+a2 = np.loadtxt("lab2.txt", skiprows = 1)
+
