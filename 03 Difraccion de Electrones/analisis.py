b13d2895d3964b63ea389236175c2c69d34488ba
Javier Acevedo <Javier Acevedo>
2017-09-28 01:55:34 -0500

    casi todo el analisis 3

--- 03 Difraccion de Electrones/analisis.py
+++ 03 Difraccion de Electrones/analisis.py
@@ -7,6 +7,7 @@ Created on Tue Sep 26 20:54:24 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 data0 = np.loadtxt("m-c1.txt")
 data = np.loadtxt("m-c2.txt")
@@ -32,6 +33,8 @@ def lbrog(U):
 def di(pend):
     return 2*h*L/(pend*np.sqrt(2*m*e))
 
+def hi(pend,d):
+    return d*np.sqrt(2*m*e)*pend/(2*L)
 
 print "Voltaje [kV] & \lambda_{deBroglie} [pm]&\lambda_{Bragg}_1 [pm] & Error \% & \lambda_{Bragg}_2 [pm] & Error \% \\\\ \\hline"
 for i in range(7):
@@ -45,23 +48,48 @@ for i in range(7):
 
 
 
-reg1 = np.polyfit((np.power(data[:,0]*1000,-0.5)), data[:,3]/100,1)
-reg2 = np.polyfit((np.power(data[:,0]*1000,-0.5)), data[:,4]/100,1)
+#reg1 = np.polyfit((np.power(data[:,0]*1000,-0.5)), data[:,3]/100,1)
+#reg2 = np.polyfit((np.power(data[:,0]*1000,-0.5)), data[:,4]/100,1)
 
+qw1,qw2,qw3,qw4,qw5 = stats.linregress((np.power(data[:,0]*1000,-0.5)), data[:,3]/100)
+ew1,ew2,ew3,ew4,ew5 = stats.linregress((np.power(data[:,0]*1000,-0.5)), data[:,4]/100)
 
-print reg1
-print reg2
+#print qw1,qw2,qw3,qw4,qw5
+#print ew1,ew2,ew3,ew4,ew5
 
-print di(reg1[0])*1e12
-print di(reg2[0])*1e12
+print "%.4f, %.5f, %.5f" % (qw1,qw2,qw3)
+print "%.4f, %.5f, %.5f" % (ew1,ew2,ew3)
 
-        
+print di(qw1)*1e12
+print di(ew1)*1e12
+
+print ""
+print hi(qw1,d1)
+print hi(ew1,d2)
+print str(hi(qw1,d1)/2.0+hi(ew1,d2)/2.0)
+
 #        
-#fig0 = plt.figure()
-#plt.scatter(np.power(data[:,0]*1000,-0.5), data[:,3]/100)
-#plt.scatter(np.power(data[:,0]*1000,-0.5), data[:,4]/100, color = 'r')
-#plt.scatter([0],[0], color = 'black')
-#nuevo = np.array(np.append([0],np.power(data[:,0]*1000,-0.5)))
-#plt.plot(nuevo, reg1[0]*nuevo+reg1[1])
-#plt.plot(nuevo, reg2[0]*nuevo+reg2[1])
-#
+fig0 = plt.figure()
+plt.scatter(np.power(data[:,0]*1000,-0.5), data[:,3]/100, color = 'g', label = "Datos D Interior ")
+plt.scatter(np.power(data[:,0]*1000,-0.5), data[:,4]/100, color = 'r', label = "Datos D exterior")
+plt.scatter([0],[0], color = 'black')
+nuevo = np.array(np.append([0],np.power(data[:,0]*1000,-0.5)))
+plt.plot(nuevo, qw1*nuevo+qw2, label = "Regresion D interno")
+plt.plot(nuevo, ew1*nuevo+ew2)
+
+l1= plt.legend(loc='center left', bbox_to_anchor=(0.8, 0.5))
+plt.savefig("rvsU.png",bbox_inches='tight')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
